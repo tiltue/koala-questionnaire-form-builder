fcf737ea1271f0f135a9a5031bb982baee565167
Til Gruenwaldt <tilgruenwaldt@gmail.com>
2025-11-23 13:52:54 +0100

    Add route for editor page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import FrontPage from '../views/FrontPage';
 import AuthCallback from '../views/AuthCallback';
+import FormBuilder from '../views/FormBuilder';
 import { TreeContextProvider } from '../store/treeStore/treeStore';
 
 export default function Routes(): JSX.Element {
@@ -9,6 +10,11 @@ export default function Routes(): JSX.Element {
         <Switch>
             <Route path="/code" exact>
                 <AuthCallback />
+            </Route>
+            <Route path="/editor" exact>
+                <TreeContextProvider>
+                    <FormBuilder />
+                </TreeContextProvider>
             </Route>
             <Route path="/" exact>
                 <TreeContextProvider>
